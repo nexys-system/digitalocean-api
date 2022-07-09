6acac73cff2f703c05be60206b6e12f26f467937
John B <johan@nexys.ch>
2022-07-09 15:25:54 +0200

    Update databases.ts

--- src/databases.ts
+++ src/databases.ts
@@ -4,7 +4,7 @@ import * as T from "./type";
 
 const host = "https://api.digitalocean.com/v2";
 
-export const request = async <A>(
+export const preRequest = async <A>(
   path: string,
   token: string,
   method: "GET" | "POST" | "DELETE" = "GET",
@@ -16,8 +16,16 @@ export const request = async <A>(
     "content-type": "application/json",
   };
   const body = data ? JSON.stringify(data) : undefined;
-  console.log(body);
-  const r = await fetch(url, { headers, body, method });
+  return fetch(url, { headers, body, method });
+};
+
+export const request = async <A>(
+  path: string,
+  token: string,
+  method: "GET" | "POST" | "DELETE" = "GET",
+  data: any = undefined
+): Promise<A> => {
+  const r = await preRequest(path, token, method, data);
 
   return r.json();
 };
@@ -31,5 +39,7 @@ export const list = async (token: string) => {
 export const create = async (data: T.DatabaseCreate, token: string) =>
   request("/databases", token, "POST", data);
 
-export const deleteByUuid = async (uuid: string, token: string) =>
-  request("/databases/" + uuid, token, "DELETE");
+export const deleteByUuid = async (uuid: string, token: string) => {
+  const r = await preRequest("/databases/" + uuid, token, "DELETE");
+  return r.text();
+}
